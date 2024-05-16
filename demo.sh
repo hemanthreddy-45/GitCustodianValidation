fcd36765a60c307f65aaa237cc51eba3dd88e699
hemanthreddy-45 <hemanth.kumar@opsera.io>
2024-05-16 11:57:08 +0530

    validating

--- demo.sh
+++ demo.sh
@@ -5,3 +5,16 @@ git checkout $TargetBraNCH
 GH_Token=ghp_KLMKJDsdfsdiewneflfc
 AWS_Password=asdajndoefnldsanasCASLDMCALASDNLASND
 password=aksjne@!a;sda88
+TargetBraNCH='develop-test'
+password="qwertyasdawda"
+AWS_ACCESS_KEY="ASDUHAEUR73923MSA9"
+git checkout $TargetBraNCH
+AWS_SECRET_ACCESS_KEY="qwerty"
+GH_Token=ghp_KLMKJDsdfsdiewneflfc
+password="qwerty"
+AWS_Password=asdajndoefnldsanasCASLDMCALASDNLASND
+password = Che%&187
+token = "hem1234"
+password = qwertyy$
+token = "P$5aRt#"
+token = P$5aRt#2
